d391d38e21f0ed4255675142aa4f207f222cf5a7
andiharun29 <andiharunalrasyid14@gmail.com>
2025-01-28 17:49:08 +0700

    Menambahkan ViewModel dan UI state untuk detail pekerja

---
+++ app/src/main/java/com/example/uas_pam/ui/viewmodel/Pekerja/PekerjaDetailViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.uas_pam.ui.viewmodel.Pekerja
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas_pam.data.Repository.PekerjaRepository
+import com.example.uas_pam.data.Repository.TanamanRepository
+import com.example.uas_pam.data.model.Pekerja
+import com.example.uas_pam.data.model.Tanaman
+import com.example.uas_pam.ui.view.pekerja.DestinasiDetailPekerja
+import com.example.uas_pam.ui.view.tanaman.DestinasiDetailTanaman
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+
+sealed class PekerjaDetailUiState {
+    data class Success(val pekerja: Pekerja) : PekerjaDetailUiState()
+    object Error : PekerjaDetailUiState()
+    object Loading : PekerjaDetailUiState()
+}
+
+class PekerjaDetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val pekerjaRepository: PekerjaRepository
+): ViewModel(){
+    private val idpekerja: String = checkNotNull(savedStateHandle[DestinasiDetailPekerja.id_pekerja])
+
+    var pekerjaDetailState: PekerjaDetailUiState by mutableStateOf(PekerjaDetailUiState.Loading)
+        private set
+
+    init {
+        getPekerjaById()
+    }
+
+
+}
