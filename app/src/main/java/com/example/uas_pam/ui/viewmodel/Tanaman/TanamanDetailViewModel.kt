3254e2bdfa5e79bf8c26c681145362687936f722
andiharun29 <andiharunalrasyid14@gmail.com>
2025-01-28 12:07:51 +0700

    tambahkan TanamanDetailViewModel dan state untuk detail tanaman

---
+++ app/src/main/java/com/example/uas_pam/ui/viewmodel/Tanaman/TanamanDetailViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.uas_pam.ui.viewmodel.Tanaman
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas_pam.data.Repository.TanamanRepository
+import com.example.uas_pam.data.model.Tanaman
+import com.example.uas_pam.ui.view.tanaman.DestinasiDetailTanaman
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class TanamanDetailUiState {
+    data class Success(val tanaman: Tanaman) : TanamanDetailUiState()
+    object Error : TanamanDetailUiState()
+    object Loading : TanamanDetailUiState()
+}
+
+class TanamanDetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val tanamanRepository: TanamanRepository
+): ViewModel(){
+    private val idtanaman: String = checkNotNull(savedStateHandle[DestinasiDetailTanaman.id_tanaman])
+
+    var tanamanDetailState: TanamanDetailUiState by mutableStateOf(TanamanDetailUiState.Loading)
+    private set
+
+    init {
+        getTanamanById()
+    }
+
+
+}
+
+
